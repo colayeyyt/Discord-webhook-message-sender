d1d8a1224e0c4cac0c3cd1650a90dcc23d6ab62f
cola yey <124573291+colayeyyt@users.noreply.github.com>
2023-06-21 20:37:06 +0900

    the main.py file

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+print('Discord wehbook sender')
+
+print('\n')
+
+#User inputs
+webhook = input('Enter your webhook: ')
+message = input('Enter your message: ')
+
+#Sending the discord message
+def send_discord_message(webhook_url, message):
+    data = {
+        'content': message
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
+    if response.status_code == 204:
+        print('Message sent successfully.')
+    else:
+        print(f'Failed to send message. Status code: {response.status_code}')
+        
+send_discord_message(webhook, message)
